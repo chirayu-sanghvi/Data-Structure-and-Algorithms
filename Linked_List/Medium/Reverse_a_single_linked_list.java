ca1564730e2e6c64e1653e3b92a2680fa2600317
chirayu-sanghvi <54971056+chirayu-sanghvi@users.noreply.github.com>
2023-11-28 07:03:35 -0500

    Update Reverse_a_single_linked_list.java

--- Linked_List/Medium/Reverse_a_single_linked_list.java
+++ Linked_List/Medium/Reverse_a_single_linked_list.java
@@ -1,6 +1,39 @@
 // Question Link: https://www.codingninjas.com/studio/problems/reverse-linked-list_920513
-
+// Leet Code: https://leetcode.com/problems/reverse-linked-list/
 /****************************************************************
+Leet code problem statement: 
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+ 
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+***********************************************************************************************************
+Coding Ninja Problem statement: 
+
 Sample Input 1 :
 1 2 4 -1
 Sample Output 1 :
