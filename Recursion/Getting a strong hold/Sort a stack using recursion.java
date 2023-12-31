fde087a2ed9c2b1bae4df5d166079e8f62329ea1
chirayu-sanghvi <54971056+chirayu-sanghvi@users.noreply.github.com>
2023-12-31 17:45:04 -0500

    Update Sort a stack using recursion.java

--- Recursion/Getting a strong hold/Sort a stack using recursion.java
+++ Recursion/Getting a strong hold/Sort a stack using recursion.java
@@ -1,7 +1,10 @@
+/*
+Coding Ninja: https://www.codingninjas.com/studio/problems/sort-stack_1229505
+*/
 import java.util.*;
 
 public class solution {
-    public void static main(String args[]) {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while(t!=0){
@@ -11,7 +14,7 @@ public class solution {
             for(int i=0; i<n; i++){
                 st.push(sc.nextInt());
             }
-            sorted_stack = sort(st);
+            st = sort(st);
             while(!st.isEmpty()){
                 int temp = st.pop();
                 System.out.print(temp+"");
@@ -21,17 +24,17 @@ public class solution {
         }
     }
 
-    public static Stack<Integer> sort(Stack<Integer> st, int x){
+    public static Stack<Integer> sort(Stack<Integer> st){
         if(st.size() == 1){
             return st;
         }
         int temp = st.pop();
-        sort(st,temp);
+        sort(st);
         insert(st,temp);
         return st;
     }
 
-    public statci Stack<Integer> inset(Stack<Integer> st, int x){
+    public static Stack<Integer> insert(Stack<Integer> st, int x){
         if(!st.isEmpty() || st.peek()>= x){
             st.push(x);
             return st;
