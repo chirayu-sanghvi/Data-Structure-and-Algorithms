4080e1d2eda718acebf0bc0647d37fee7d82aa9d
chirayu-sanghvi <54971056+chirayu-sanghvi@users.noreply.github.com>
2024-01-02 11:58:59 -0500

    Create Subarray with sum k.java

---
+++ Recursion/Subarray with sum k.java
@@ -0,0 +1,48 @@
+/*
+Coding Ninja: https://www.codingninjas.com/studio/problems/subarrays-with-sum-%E2%80%98k'_6922076
+
+*/
+import java.util.*;
+public class Solution {
+public static List< List < Integer > > subarraysWithSumK(int []a, long k) {
+        int n = a.length;
+        List< List< Integer > > ans = new ArrayList<>();
+        // This will keep all those sub-arrays whose sum = 'k'.
+
+        // start and end are the starting
+        // and the ending indices of our current
+        // subarray
+        int start = 0, end = -1;
+        long currentSum = 0;
+
+        // Iterating over 'a'
+        while (start < n) {
+
+            // Increasing the right end
+            // till our sum <= 'k' or we
+            // are not out of bounds
+            while ((end + 1 < n) && (currentSum + a[end + 1] <= k)) {
+                currentSum += a[end + 1];
+                ++end;
+            }
+
+            // We have found a subarray with the
+            //  required sum.
+            if (currentSum == k) {
+                List< Integer > temp = new ArrayList<>();
+
+                for(int i = start; i <= end; ++i) {
+                    temp.add(a[i]);
+                }
+                ans.add(new ArrayList<>(temp));
+            }
+
+            // Shifting the start index
+            currentSum -= a[start];
+            ++start;
+        }
+
+        return ans;
+    }
+}
+    
