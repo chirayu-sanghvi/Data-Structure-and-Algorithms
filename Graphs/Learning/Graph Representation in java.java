90e6c0ba9f5c7c817c2a8ceac4b9ceb7ef045196
chirayu-sanghvi <54971056+chirayu-sanghvi@users.noreply.github.com>
2024-01-06 05:25:51 -0500

    Create Graph Representation in java.java

---
+++ Graphs/Learning/Graph Representation in java.java
@@ -0,0 +1,83 @@
+/*
+Coding Ninja: https://www.codingninjas.com/studio/problems/creating-and-printing_1214551
+
+Problem statement
+You are given an undirected graph of 'N' nodes and 'M' edges. Your task is to create the graph and print the adjacency list of the graph. It is guaranteed that all the edges are unique, i.e., if there is an edge from 'X' to 'Y', then there is no edge present from 'Y' to 'X' and vice versa. Also, there are no self-loops present in the graph.
+
+
+
+In graph theory, an adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a vertex in the graph.
+
+
+
+For Example:
+If 'N' = 3 and edges = {{2,1}, {2,0}}.
+
+So, the adjacency list of the graph is stated below.
+0 → 2
+1 → 2
+2 → 0 → 1
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1:
+4 3
+1 2
+0 3
+2 3
+Sample Output 1:
+0 3
+1 2
+2 1 3
+3 0 2
+Explanation For Sample Input 1:
+The graph is shown in the below figure.
+
+So, the neighbour of node 0 is 3. So, in the first line, the first integer is 0 followed by its neighbour 3. Similarly in the second line, 1 is followed by its neighbour 2. 
+
+In the third line, 2 is followed by its neighbours 1 and 3. And in the fourth line, 3 is followed by its neighbours 0 and 2.
+Sample Input 2:
+3 3
+0 1
+1 2
+2 0
+Sample Output 2:
+0 1 2
+1 0 2
+2 0 1
+Explanation For Sample Input 2:
+The graph is shown in the below figure.
+
+So, the neighbour of node 0 is 1 and 2. So, in the first line, the first integer is 0 followed by its neighbour 1 and 2. Similarly in the second line, 1 is followed by its neighbour 2 and 0. 
+
+In the third line, 2 is followed by its neighbours 1 and 0. 
+Constraints:
+1 <= N <= 5000
+1 <= M <= min(5000, (N * (N - 1)) / 2)
+0 <= edges[i][0], edges[i][1] <= N-1
+
+Time limit: 1 sec
+*/
+
+import java.util.*;
+public class Solution {
+    public static int[][] printAdjacency(int n, int m, int[][] edges) {
+        // Write your code here.
+        ArrayList<ArrayList<Integer>> alist = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            alist.add(new ArrayList<>());
+            alist.get(i).add(i);
+        }
+
+        for(int k[]: edges){
+            int u = k[0];
+            int v = k[1];
+            alist.get(u).add(v);
+            alist.get(v).add(u);
+        }
+
+        int answer[][] = new int[n][];
+        for(int i=0;i<n;i++){
+            answer[i] = alist.get(i).stream().mapToInt(Integer::intValue).toArray();
+        }
+        return answer;
+    }
+}
